ceec125b9eb0e541d42c5d6eea6ebaefc6f0923b
katet0k <katakostucenko0@gmail.com>
2022-10-15 10:37:52 +0300

    update:feature/lab_ex2

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-height = int(input('->'))
-length = int(input('->'))
+try:
+   height = int(input('->'))
+   length = int(input('->'))
 
-for i in range(height):
-    for j in range(length):
-        print('*', end='')
-    print()
+   for i in range(height):
+       for j in range(length):
+           print('*', end='')
+       print()
+except Exception as ex:
+    print(f'Error: {ex}')

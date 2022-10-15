d4bd7e2e4818bb12663719a049702e4a1ad03760
katet0k <katakostucenko0@gmail.com>
2022-10-15 10:59:15 +0300

    update:feature/lab_ex3

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-import sys
 try:
     size = int(input('->'))
 
     for i in range(size):
         for j in range(size):
             if i == 0 or j == 0 or i == size-1 or j == size-1:
-                sys.stdout.write('*')
+                print('* ', end=' ')
             else:
-                sys.stdout.write(' ')
+                print('  ', end=' ')
         print(' ')
 except Exception as ex:
     print(f'Error: {ex}')

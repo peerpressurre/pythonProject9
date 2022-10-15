8c2a71a221aa94b449eb91541075fb7d7c40f055
katet0k <katakostucenko0@gmail.com>
2022-10-15 10:32:49 +0300

    update:feature/lab_ex3

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-height = int(input('->'))
-length = int(input('->'))
+try:
+    size = int(input('->'))
+    size += 1
 
-for i in range(height):
-    for j in range(length):
-        print('*', end='')
+    for i in range(size):
+        for j in range(size):
+            if i == 0:
+                print('i0', end='')
+            else:
+                if j == {size}:
+                    print('js', end='')
+                else:
+                    if j == 0:
+                        print('\nj0')
+                    else:
+                        if i == {size}:
+                            print('is', end='')
+                        else:
+                            print('')
     print()
+except Exception as ex:
+    print(f'Error: {ex}')

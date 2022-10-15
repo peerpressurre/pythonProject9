ae12d2e42ae854e135fa38d9fa1c4e62486a36c8
katet0k <katakostucenko0@gmail.com>
2022-10-15 10:33:36 +0300

    update:feature/lab_ex1

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-size = int(input('->'))
+try:
+   size = int(input('->'))
 
-for i in range(size):
-    for j in range(size):
-        print('*', end='')
-    print()
+   for i in range(size):
+       for j in range(size):
+           print('*', end='')
+   print()
+except Exception as ex:
+    print(f'Error: {ex}')

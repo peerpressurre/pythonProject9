aaa29f49a2c371025584f8cc4f843b485e0c43c5
katet0k <katakostucenko0@gmail.com>
2022-10-15 11:01:17 +0300

    update:feature/lab_ex4

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import sys
 try:
     length = int(input('->'))
     width = int(input('->'))
@@ -6,9 +5,9 @@ try:
     for i in range(length):
         for j in range(width):
             if i == 0 or j == 0 or i == length-1 or j == width-1:
-                sys.stdout.write('*')
+                print('* ', end=' ')
             else:
-                sys.stdout.write(' ')
+                print('  ', end=' ')
         print(' ')
 except Exception as ex:
     print(f'Error: {ex}')

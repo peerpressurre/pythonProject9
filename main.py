5e398c857a28580326b548377862845406c0edda
katet0k <katakostucenko0@gmail.com>
2022-10-15 10:55:04 +0300

    update:feature/lab_ex4

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import sys
 try:
-    size = int(input('->'))
-    size += 1
+    length = int(input('->'))
+    width = int(input('->'))
 
-    for i in range(size):
-        for j in range(size):
-            if i == 0 or j == 0 or i == size-1 or j == size-1:
+    for i in range(length):
+        for j in range(width):
+            if i == 0 or j == 0 or i == length-1 or j == width-1:
                 sys.stdout.write('*')
             else:
                 sys.stdout.write(' ')
